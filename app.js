1425943a32ad78439ca044ac513a59ff3ad1c01d
Caroline-Vendramini <caroline.vendramini07@gmail.com>
2024-01-22 18:25:07 -0300

    busca de usuários na lista de usuários

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 const express = require('express')
 const app = express()
+app.use(express.json())
 const port = 3000
+
+const users = [
+    { 
+        email: "abc@gmail.com",
+        senha: "123",
+        nome: "Maria",
+    },
+    {
+        email: "def@gmail.com",
+        senha: "000",
+        nome: "João",
+    },
+    {
+        email: "ghi@gmail.com",
+        senha: "111",
+        nome: "José",
+    }
+]
 
 app.get('/', (req, res) => {
     res.send('Olá Mundo!')
@@ -11,7 +30,21 @@ app.get('/bemvindo', (req, res) => {
 })
 
 app.post('/login', (req, res) => {
-    res.send('!')
+    const email = req.body.email
+    const senha = req.body.senha
+    console.log(senha)
+    console.log(email)
+    for (var i= 0; i< users.length; i= i+1) {
+        const user = users[i]
+        if (email === user.email) {
+            if (senha === user.senha) {
+                return res.json(user)
+            }
+        }
+    
+    }
+    return res.send("credenciais inválidas")
+
 })
 
 app.listen(port, () => {
